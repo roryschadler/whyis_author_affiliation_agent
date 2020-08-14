2fc360f69bd54bc2bd99fe680e61c28172635a7c
Rory Schadler <Rory.H.Schadler.21@dartmouth.edu>
2020-08-14 14:53:54 -0400

    set up for packaging

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="whyis_author_affiliation_agent",
+    version="0.0.1",
+    author="Rory Schadler",
+    author_email="rory.h.schadler.21@dartmouth.edu",
+    description="Author and Affiliation retrieval for Whyis",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/roryschadler/whyis_author_affiliation_agent",
+    packages=['whyis_author_affiliation_agent'],
+    install_requires=['rdflib',
+                      'arcgis'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.5.2'
+)
